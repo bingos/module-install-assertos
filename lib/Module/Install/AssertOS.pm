befe86cb169d7168396a3611792656845949d25f
Chris Williams <chris@bingosnet.co.uk>
2009-04-23 20:44:13 +0100

    Refactored the author side into a separate sub

--- lib/Module/Install/AssertOS.pm
+++ lib/Module/Install/AssertOS.pm
@@ -6,7 +6,7 @@ use base qw(Module::Install::Base);
 use File::Spec;
 use vars qw($VERSION);
 
-$VERSION = '0.04';
+$VERSION = '0.06';
 
 sub assertos {
   my $self = shift;
@@ -18,6 +18,12 @@ sub assertos {
      Devel::AssertOS->import( @oses );
      return;
   }
+
+  _author_side( @oses );
+}
+
+sub _author_side {
+  my @oses = @_;
 
   require Data::Compare;
 
